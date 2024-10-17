65a5af7d8b9db176906713df6446571ad25af40a
tweygg33 <tweygg@gmail.com>
2024-10-17 13:15:15 +0200

    a

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ async function kameraAuswahl(){
         initialStream.getTracks().forEach(track => track.stop());
         //Liste aller Geräte intern
         const devices = await navigator.mediaDevices.enumerateDevices();
+        console.log(videoInputDevices)
         //Filtern geräte nur auf Video
         let videoInputDevices = devices.filter(device => device.kind === 'videoinput');
         console.log(videoInputDevices)
