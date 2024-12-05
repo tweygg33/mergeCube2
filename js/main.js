9ee1f65cb091e34494bda89e4dec678b243333fa
tweygg33 <tweygg@gmail.com>
2024-12-05 12:59:14 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,28 +7,22 @@ AFRAME.registerComponent('rotation-order', {
         this.el.object3D.rotation.order = this.data;
     }
 });
-
 // Initialisieren aller Touch-Funktionen
 touchaktionen.alleFunktionen();
-
 // Globale Variablen initialisieren
 globale_variablen.samsung_check();
-
 window.onload = () => {
-    initCameraSelection();
-    
+    initCameraSelection();  
     // Verhindern von ungewollten Zoom-Interaktionen
     document.addEventListener('touchmove', function(event) {
         if (event.scale !== 1) {
             event.preventDefault();
         }
     }, { passive: false });
-
     // Verhindern von Doppeltipp-Zoom
     document.addEventListener('dblclick', function(event) {
         event.preventDefault();
     }, { passive: false });
-
     // Verhindern von Zoom mit Strg+Mausrad
     document.addEventListener('wheel', function(event) {
         if (event.ctrlKey) {
@@ -36,7 +30,6 @@ window.onload = () => {
         }
     }, { passive: false });
 };
-
 // Funktion zur Initialisierung der Kameraauswahl
 async function initCameraSelection() {
     try {
